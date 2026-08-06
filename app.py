e9b30f488d6743bcc491424d9715cbc8fb7e47a8
AURYSS <ceciaurora2017@gmail.com>
2026-08-06 11:52:53 -0600

    cambio

--- app.py
+++ app.py
@@ -225,7 +225,7 @@ if pantalla == "1. Ingesta de Datos":
     col1, col2 = st.columns([2, 1])
     
     with col1:
-        st.write("Suba el archivo CSV con las respuestas Likert (`p1`-`p15`) y respuestas abiertas (`p1a`-`p15a`) para insertarlas en la base de datos PostgreSQL local `zodiac`.")
+        st.write("Suba el archivo CSV con las respuestas Likert (`p1`-`p15`) y respuestas abiertas (`p1a`-`p15a`).")
         uploaded_file = st.file_uploader("Subir dataset en formato .csv", type=["csv"])
         
         if uploaded_file is not None:
